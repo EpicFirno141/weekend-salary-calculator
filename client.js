b830dbef4fa502868f8cfce696230c0ba20580ff
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-20 19:31:36 -0600

    Finally finished calculator portion... including recalculations after removal of an entry.

--- client.js
+++ client.js
@@ -1,3 +1,9 @@
+let entryData = []
+entryData.push(35000);
+entryData.push(42000);
+console.log(entryData);
+calculateSalary();
+
 function recordData(event) {
     event.preventDefault();
     let firstName = document.querySelector('#firstNameValue').value;
@@ -16,8 +22,39 @@ function recordData(event) {
         <td><button onClick="removeEntry(event)">Delete</button></td>
     </tr>
     `;
+    salaryArrayAdd(salary);
+    calculateSalary();
+}
+
+function salaryArrayAdd(salary) {
+    let editedNumber = salary.replace(',','');
+    entryData.push(Number(editedNumber));
 }
 
 function removeEntry(event) {
+    let removedSalary = event.target.parentElement.parentElement.getElementsByTagName('td')[4].innerHTML;
+    let splitText = removedSalary.split(' ');
+    let finishedNumber = splitText[1].replace(',','');
+    let arrayEntry = entryData.indexOf(Number(finishedNumber));
+    console.log('Removing: ' + finishedNumber + ' with an index of: ' + arrayEntry);
+    entryData.splice(arrayEntry, 1);
+    console.log('Array now contains: ' + entryData);
+    calculateSalary();
     event.target.parentElement.parentElement.remove();
+}
+
+function calculateSalary(){
+    let salaryTotal = 0;
+    for(let salary of entryData){
+        salaryTotal += salary;
+    }
+    let monthlyCost = salaryTotal/12;
+    let monthlyDiv = document.querySelector('#calculatorResult');
+    monthlyDiv.innerHTML = `$ ${numberRound(monthlyCost)}`;
+}
+
+function numberRound(number) {
+    let rounded = Math.round(number*100);
+    let finishRound = rounded/100;
+    return finishRound;
 }
