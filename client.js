1d04a45ceaac4e12e57b544fe522cc9ddc3e7436
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-20 18:38:38 -0600

    Finished making function that would append data to the dom

--- client.js
+++ client.js
@@ -1,3 +1,23 @@
 function recordData(event) {
-    
+    event.preventDefault();
+    let firstName = document.querySelector('#firstNameValue').value;
+    let lastName = document.querySelector('#lastNameValue').value;
+    let id = document.querySelector('#idValue').value;
+    let title = document.querySelector('#titleValue').value;
+    let salary = document.querySelector('#salaryValue').value;
+    let tableDiv = document.querySelector('#tableData');
+    tableDiv.innerHTML += `
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>$ ${salary}</td>
+        <td><button onClick="removeEntry(event)">Delete</button></td>
+    </tr>
+    `;
+}
+
+function removeEntry(event) {
+
 }
